2a7b4c4b935fb0bb1a9c502025ccdb1c613ae810
AlejandroMedina8 <diegoparra268@gmail.com>
2025-04-14 22:13:01 -0500

    Perfeccionando tu e-commerce

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,32 +1,30 @@
 import { Routes } from '@angular/router';
 
-import { ListComponent } from '@products/pages/list/list.component';
-import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
 import { LayoutComponent } from '@shared/components/layout/layout.component';
-import { AboutComponent } from '@info/pages/about/about.component'
-import { NotFoundComponent } from '@info/pages/not-found/not-found.component'
+import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
         path: '',
-        component: LayoutComponent,
-        children: [
-            {
-                path: '',
-                component: ListComponent
-            },
-            {
-                path: 'about',
-                component: AboutComponent
-            },
-            {
-                path: 'product/:id',
-                component: ProductDetailComponent
-            }
-        ]
-    },
-    {
-        path: '**',
-        component: NotFoundComponent
-    }
+        loadComponent: () => import('@products/pages/list/list.component'),
+      },
+      {
+        path: 'about',
+        loadComponent: () => import('@info/pages/about/about.component'),
+      },
+      {
+        path: 'product/:id',
+        loadComponent: () =>
+          import('@products/pages/product-detail/product-detail.component'),
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
